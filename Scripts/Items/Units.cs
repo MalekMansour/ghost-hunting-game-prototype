88fe05b50df603cf9739c3fb82b28363560f3765
Malek Mansour <138655005+MalekMansour@users.noreply.github.com>
2025-12-26 15:19:11 -0700

    Add Units class for environmental properties

---
+++ Scripts/Items/Units.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class Units : MonoBehaviour
+{
+    [Header("Environmental Units")]
+    public float light = 0f;
+    public float noise = 0f;
+    public float heat = 0f;
+    public float cold = 0f;
+    public float smell = 0f;
+    public float food = 0f;
+    public float water = 0f;
+}
