bf31a499957aec34bceac7accb51afa26d6f2665
Malek Mansour <138655005+MalekMansour@users.noreply.github.com>
2025-12-26 15:14:26 -0700

    Implement player raycasting for item interaction

--- Scripts/PlayerRaycaster.cs
+++ Scripts/PlayerRaycaster.cs
@@ -1,1 +1,48 @@
+using UnityEngine;
+
+public class PlayerRaycaster : MonoBehaviour
+{
+    [Header("Raycast")]
+    public float interactDistance = 3f;
+    public LayerMask itemLayer;
+    public float raycastInterval = 0.1f;
+
+    [HideInInspector] public bool hasHit;
+    [HideInInspector] public RaycastHit hit;
+
+    private Camera cam;
+    private float raycastTimer;
+
+    void Start()
+    {
+        cam = Camera.main;
+    }
+
+    void Update()
+    {
+        raycastTimer -= Time.deltaTime;
+
+        if (raycastTimer <= 0f)
+        {
+            raycastTimer = raycastInterval;
+            DoRaycast();
+        }
+    }
+
+    void DoRaycast()
+    {
+        hasHit = false;
+
+        if (cam == null)
+            return;
+
+        Ray ray = new Ray(cam.transform.position, cam.transform.forward);
+
+        if (Physics.Raycast(ray, out RaycastHit rayHit, interactDistance, itemLayer))
+        {
+            hasHit = true;
+            hit = rayHit;
+        }
+    }
+}
 
